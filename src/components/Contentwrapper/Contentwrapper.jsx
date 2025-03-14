d69c0ae83ec852c111bf8d6bc6b60d7bcb75c2bd
Devendra Deore <1002deoredk@gmail.com>
2025-03-14 17:21:29 +0530

    mobile view responsive

--- src/components/Contentwrapper/Contentwrapper.jsx
+++ src/components/Contentwrapper/Contentwrapper.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Aboutus from "../../pages/Aboutus/Aboutus";
-import Process from "../../pages/Process/Process";
+
 function Contentwrapper({ children, className }) {
   return (
     <div className={`w-full max-w-[1440px] mx-auto my-0 py-0 ' +  {className}`}>
